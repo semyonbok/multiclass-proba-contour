0e3d56cf9e388d65eaf06ccb0d3ddca36aedfe5b
semyonbok <semyon.bok@gmail.com>
2023-06-29 21:48:12 +0100

    unresponsive model-specific widgets for panel app

--- ProbaVis_panel_app.py
+++ ProbaVis_panel_app.py
@@ -1,10 +1,11 @@
+import param
 import panel as pn
 from sklearn.datasets import load_iris, load_wine
 
 import multiclass_proba_contour as mpc
 
 pn.extension(template="material")
-# panel serve ProbaVis_panel_app_master.py --show --autoreload
+# panel serve ProbaVis_panel_app.py --show --autoreload
 
 # Widget options
 all_datasets = ["Iris", "Wine"]
@@ -31,10 +32,11 @@ f1_widget = pn.widgets.Select(
 f2_widget = pn.widgets.Select(
     name="Pick the second feature", options=iris_features[1:], value=iris_features[1]
     )
-update_button = pn.widgets.Button(name='Update plot', button_type='primary')
-model_widget = pn.widgets.Select(name="Select one of the models", options=all_models)
+model_widget = pn.widgets.Select(
+    name="Select one of the models", options=all_models
+    )
 
-# Define a callback functions that updates the options of f1_widget and f2_widget
+# Callback functions that updates the downstream widgets
 def update_features(event):
     if event.new == "Iris":
         f1_widget.options = iris_features
@@ -48,14 +50,43 @@ def update_f2(event):
     f2_options.remove(event.new)
     f2_widget.options = f2_options
 
-# Attach the callbacks to the 'value' parameter
+knn_hyper_widgets = {
+    "n_neighbors" : pn.widgets.IntSlider(
+        name="n_neighbors", value=5, start=1, end=100),
+    "algorithm" : pn.widgets.Select(
+        name="algorithm", options=['auto', 'ball_tree', 'kd_tree', 'brute']),
+        }
+
+class HyperWidgetHolder(param.Parameterized):
+    hyper_widgets = param.Parameter(default=knn_hyper_widgets)
+
+holder = HyperWidgetHolder()
+
+def create_hyper_widgets(event):
+    if event.new == "K Nearest Neighbors":
+        holder.hyper_widgets = knn_hyper_widgets
+    elif event.new == "Random Forest":
+        holder.hyper_widgets = {
+            "criterion" : pn.widgets.Select(
+                name="criterion", options=['gini', 'entropy']),
+            "n_estimators" : pn.widgets.IntSlider(
+                name="n_estimators", value=100, start=1, end=500),
+        }
+
+# Attach the callbacks to the 'value' parameter of appropriate widgets
 data_widget.param.watch(update_features, 'value')
 f1_widget.param.watch(update_f2, "value")
+model_widget.param.watch(create_hyper_widgets, "value")
+
+# Create a pn.panel function to display the downstream widget
+# @pn.depends(holder.param.hyper_widgets)
+# def generate_hyper_widgets(widgets):
+#     return pn.Column(*widgets.values())
 
 input_column=pn.Column()
-input_column.extend([data_widget, f1_widget, f2_widget, model_widget, update_button])
+input_column.extend([data_widget, f1_widget, f2_widget])
 
-def master_func(set_name, model, f1, f2):
+def master_func(set_name, model, f1, f2, **model_params):
     if set_name == "Wine":
         data_set = load_wine(as_frame=True)
     elif set_name == "Iris":
@@ -77,28 +108,31 @@ def master_func(set_name, model, f1, f2):
         from sklearn.ensemble import RandomForestClassifier
         model = RandomForestClassifier()
 
-    # feature_pick = lambda f, default=0: f if f is not None else default
+    model.set_params(**model_params)
     pv = mpc.ProbaVis(model, data, target, [f1, f2])
     
     return pv.plot(return_fig=True, fig_size=(9, 6))
 
+# @pn.depends(holder.param.hyper_widgets)
+# def display_widget(widget):
+#     return widget
+
 master_bind = pn.bind(
     master_func, set_name=data_widget, model=model_widget,
-    f1=f1_widget, f2=f2_widget
+    f1=f1_widget, f2=f2_widget, #**(lambda: holder.hyper_widgets)()
     )
+# pn.Column(pn.Column(display_widget)).servable()
+# def render_hyper_widgets(model):
+#     return [w for w in pn.state.cache['hyper_widgets'].values()]
 
-# Make the master_bind function depend on the click event of the update button
-@pn.depends(update_button.param.clicks)
-def update_plot(clicks):
-    return master_bind()
+hyper_bind = pn.bind(lambda x: pn.Column(*holder.hyper_widgets.values()), model_widget)
+# input_column.append(param_bind)
 
-app = pn.Row(
-    input_column,
-    pn.pane.Matplotlib(update_plot, format='svg', sizing_mode='scale_both'),
-     ).servable(title="Welcome to Multiclass Probability Visualizer!")
+master_pane = pn.pane.Matplotlib(master_bind, format='svg', sizing_mode='scale_both')
 
-# app = pn.Row(
-#     input_column,
-#     pn.pane.Matplotlib(master_bind, format='svg', sizing_mode='scale_both'),
-#     # pn.pane.Matplotlib("https://scikit-learn.org/stable/_images/sphx_glr_plot_classifier_comparison_001.png")
-#     ).servable(title="Welcome to Multiclass Probability Visualizer!")
+# app layout
+pn.Row(master_pane).servable(title="Welcome to Multiclass Probability Visualizer!")
+hyper_column = pn.Column(model_widget, hyper_bind)
+input_column.servable(target="sidebar")
+hyper_column.servable(target="sidebar")
+
